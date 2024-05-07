9ce1fb04452b1e160b78a732ad88178b0fef486e
Prkn7 <automation.qa.sele@gmail.com>
2024-05-07 18:10:00 +0530

    Initial files commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeAndroid() {
+		System.out.println("Android App");
+		
+	}
+	
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSAPPS");
+	}
+
+}
